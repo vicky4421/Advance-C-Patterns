976994a8964ffb9df51903c9e320e6eaf4162c5b
vicky <vicky210490@gmail.com>
2026-02-23 16:08:30 +0530

    comments

--- Finite State Machine Pattern/Advance Function Pointer Approach/microwave.c
+++ Finite State Machine Pattern/Advance Function Pointer Approach/microwave.c
@@ -3,18 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Forward declaration of the Microwave struct
 struct Microwave;
 
+// Define a function pointer type for state functions
 typedef void (*StateFunc)(struct Microwave* m, MicrowaveEvent e);
 
+// Define the Microwave struct with a function pointer to the current state
 struct Microwave {
   StateFunc current_state;
 };
 
+// State function prototypes
 static void state_idle(struct Microwave* m, MicrowaveEvent e);
 static void state_heating(struct Microwave* m, MicrowaveEvent e);
 static void state_paused(struct Microwave* m, MicrowaveEvent e);
 
+// State function for IDLE state
 static void state_idle(struct Microwave* m, MicrowaveEvent e) {
   if (e == EVENT_START) {
     printf("[IDLE] -> Switching to HEATING\n");
@@ -22,6 +27,7 @@ static void state_idle(struct Microwave* m, MicrowaveEvent e) {
   }
 }
 
+// State function for HEATING state
 static void state_heating(struct Microwave* m, MicrowaveEvent e) {
   if (e == EVENT_STOP) {
     printf("[HEATING] -> Switching to PAUSED\n");
@@ -32,6 +38,7 @@ static void state_heating(struct Microwave* m, MicrowaveEvent e) {
   }
 }
 
+// State function for PAUSED state
 static void state_paused(struct Microwave* m, MicrowaveEvent e) {
   if (e == EVENT_START) {
     printf("[PAUSED] -> Resuming to HEATING\n");
@@ -39,14 +46,17 @@ static void state_paused(struct Microwave* m, MicrowaveEvent e) {
   }
 }
 
+// Public API functions
 Microwave* Microwave_Create() {
   Microwave* microwave = malloc(sizeof(struct Microwave));
   if (microwave) microwave->current_state = state_idle;
   return microwave;
 }
 
+// Process events by calling the current state function
 void Microwave_Process_Event(Microwave* microwave, MicrowaveEvent event) {
   if (microwave->current_state) microwave->current_state(microwave, event);
 }
 
+// Clean up the microwave instance
 void Microwave_Destroy(Microwave* microwave) { free(microwave); }
