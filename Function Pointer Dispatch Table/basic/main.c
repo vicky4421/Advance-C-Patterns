4358a9d5e05511d4ed00b343fae7d50602c03b58
vicky <vicky210490@gmail.com>
2026-02-23 18:23:51 +0530

    comments

--- Function Pointer Dispatch Table/basic/main.c
+++ Function Pointer Dispatch Table/basic/main.c
@@ -3,14 +3,20 @@
 #include "my_program.h"
 
 int main() {
+  // Declare variables for the first number, second number, choice of operation,
+  // and result
   int first, second, choice, result;
 
+  // Initialize the first number, second number, and choice of operation
   first = 2;
   second = 3;
   choice = 1;
 
+  // Create an array of function pointers to the math functions
   math_func* func_array[] = {add, sub, mult, divide};
 
+  // Call the chosen math function using the function pointer array and store
+  // the result
   result = func_array[choice](first, second);
 
   printf("Result is %d\n", result);
@@ -18,6 +24,7 @@ int main() {
   return 0;
 }
 
+// Function definitions for basic math operations
 int add(int first, int second) { return first + second; }
 
 int sub(int first, int second) { return first - second; }
--- Function Pointer Dispatch Table/basic/my_program.h
+++ Function Pointer Dispatch Table/basic/my_program.h
@@ -1,11 +1,13 @@
 #ifndef MY_PROGRAM_H
 #define MY_PROGRAM_H
 
+// Function prototypes for basic math operations
 int add(int first, int second);
 int sub(int first, int second);
 int mult(int first, int second);
 int divide(int first, int second);
 
+// typedef for a function pointer to a math function
 typedef int math_func(int first, int second);
 
 #endif
