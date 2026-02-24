3eabbbc6bd821b1d122e51ce5692dfab45d34ee8
vicky <vicky210490@gmail.com>
2026-02-24 14:24:10 +0530

    comments

--- Function Pointer Dispatch Table/2D table/main.c
+++ Function Pointer Dispatch Table/2D table/main.c
@@ -1,6 +1,9 @@
 #include "microwave.h"
 
+// This file implements the microwave state machine using a 2D function pointer
+// dispatch table.
 int main() {
+  // Create a microwave instance
   Microwave* oven = Microwave_create();
 
   // Imagine the user pressing buttons
@@ -9,6 +12,7 @@ int main() {
   Microwave_press_button(oven, EVENT_START);       // Output: -> HEATING ON
   Microwave_press_button(oven, EVENT_TIMER_DONE);  // Output: -> BEEP! DONE!
 
+  // Clean up
   Microwave_destroy(oven);
   return 0;
 }
--- Function Pointer Dispatch Table/2D table/microwave.c
+++ Function Pointer Dispatch Table/2D table/microwave.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define the states of the microwave
 typedef enum {
   STATE_IDLE,
   STATE_HEATING,
@@ -10,40 +11,48 @@ typedef enum {
   MAX_STATES
 } MicrowaveState;
 
+// Define the Microwave struct
 struct Microwave {
   MicrowaveState current_state;
 };
 
+// Define the function pointer type for actions
 typedef void (*ActionFunc)(Microwave* m);
 
+// Action functions for doing nothing (for invalid transitions)
 static void do_nothing(Microwave* m) {}
 
+// Action function for starting the heat
 static void start_heat(Microwave* m) {
   printf("Heating ON\n");
   m->current_state = STATE_HEATING;
 }
 
+// Action function for pausing the heat
 static void pause_heat(Microwave* m) {
   printf("Heating PAUSED\n");
   m->current_state = STATE_PAUSED;
 }
 
+// Action function for finishing the cook
 static void finish_cook(Microwave* m) {
   printf("BEEP! DONE!\n");
   m->current_state = STATE_IDLE;
 }
 
+// Action function for resetting to idle from paused state
 static void reset_idle(Microwave* m) {
   printf("Reset to IDLE!\n");
   m->current_state = STATE_IDLE;
 }
 
-// 2D Array
+// 2D Array representing the state transition table
 static ActionFunc microwave_grid[MAX_STATES][MAX_EVENTS] = {
     [STATE_IDLE] = {start_heat, do_nothing, do_nothing},
     [STATE_HEATING] = {do_nothing, pause_heat, finish_cook},
     [STATE_PAUSED] = {start_heat, reset_idle, do_nothing}};
 
+// Function implementations
 Microwave* Microwave_create() {
   Microwave* m = malloc(sizeof(struct Microwave));
   if (m) m->current_state = STATE_IDLE;
@@ -51,7 +60,7 @@ Microwave* Microwave_create() {
 }
 
 void Microwave_press_button(Microwave* m, MicrowaveEvent e) {
-  if (e > MAX_EVENTS) return;
+  if (e >= MAX_EVENTS) return;
   ActionFunc action = microwave_grid[m->current_state][e];
   action(m);
 }
